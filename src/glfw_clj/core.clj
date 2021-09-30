486d26369f06426f7b7be88525c2140a3c3551f9
Joshua Suskalo <joshua@iris.tv>
2021-09-30 16:04:35 -0500

    Add functions for initialization and error handling

--- src/glfw_clj/core.clj
+++ src/glfw_clj/core.clj
@@ -1,1 +1,102 @@
-(ns glfw-clj.core)
+(ns glfw-clj.core
+  (:require
+   [coffi.mem :as mem :refer [defalias]]
+   [coffi.ffi :as ffi :refer [defcfn]]))
+
+(ffi/load-system-library "glfw")
+
+(defcfn init
+  "Initializes GLFW.
+
+  Returns true if it initializes successfully. Automatically terminates GLFW if
+  it fails. If it succeeds, then you should call [[terminate]] before
+  application exit."
+  "glfwInit" [] ::mem/int
+  glfw-init
+  []
+  (not (zero? (glfw-init))))
+
+(defcfn terminate
+  "Shuts down GLFW.
+
+  The contexts of any windows must not be current on any other thread when this
+  is called."
+  "glfwTerminate" [] ::mem/void)
+
+(def ^:private hint->glenum
+  "Map from hint keywords to the GLEnum values they represent."
+  {:joystick-hat-buttons 0x00050001
+   :cocoa-chdir-resources 0x00051001
+   :cocoa-menubar 0x00051002})
+(def ^:private hints (set (keys hint->glenum)))
+
+(defcfn init-hint
+  "Sets the given `hint` with the boolean `value`.
+
+  The `hint` must be one of the following:
+  - `:joystick-hat-buttons`
+  - `:cocoa-chdir-resources`
+  - `:cocoa-menubar`
+
+  Hints starting with `:cocoa` are MacOS-specific.
+
+  This must be called before [[init]]."
+  "glfwInitHint" [::mem/int ::mem/int] ::mem/void
+  glfw-init-hint
+  [hint value]
+  (assert (hints hint) (str "`hint` is one of " (pr-str hints)))
+  (glfw-init-hint (hints hint) (if value 1 0)))
+
+(defcfn get-version
+  "Gets a vector of the major, minor, and revision version of GLFW.
+
+  This can be called on any thread, and before [[init]]."
+  "glfwGetVersion" [::mem/pointer ::mem/pointer ::mem/pointer] ::mem/void
+  glfw-get-version
+  []
+  (with-open [scope (mem/stack-scope)]
+    (let [major (mem/alloc-instance ::mem/int scope)
+          minor (mem/alloc-instance ::mem/int scope)
+          rev (mem/alloc-instance ::mem/int scope)]
+      (glfw-get-version (mem/address-of major)
+                        (mem/address-of minor)
+                        (mem/address-of rev))
+      (mapv #(mem/deserialize-from % ::mem/int) [major minor rev]))))
+
+(defcfn get-version-string
+  "Gets a string of the version of GLFW.
+
+  Don't parse the string to get the version. Instead use [[get-version]].
+
+  This can be called on any thread, and before [[init]]."
+  "glfwGetVersionString" [] ::mem/c-string)
+
+(defcfn get-error
+  "Gets the most recent error which has occurred on this thread.
+
+  If there is an error to fetch, it is returned as an [[ex-info]] with the
+  message as the error description and the key `:code` in the [[ex-data]] with
+  the error code.
+
+  This function may be called before [[init]]."
+  "glfwGetError" [::mem/pointer] ::mem/int
+  glfw-get-error
+  []
+  (with-open [scope (mem/stack-scope)]
+    (let [description (mem/alloc-instance ::mem/pointer scope)
+          error-code (glfw-get-error (mem/address-of description))
+          description-str (mem/deserialize-from description ::mem/c-string)]
+      (when-not (zero? error-code)
+        (ex-info description-str {:code error-code})))))
+
+(defalias ::error-fn [::ffi/fn [::mem/int ::mem/c-string] ::mem/void])
+
+(defcfn set-error-callback
+  "Sets the global error callback for GLFW.
+
+  The callback is a function of an integer error code and a string description.
+
+  Returns the previous callback, or nil if there was none.
+
+  This function may be called before [[init]]."
+  "glfwSetErrorCallback" [::error-fn] ::error-fn)
